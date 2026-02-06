6cf3f2b08237289571e776916edc0000e0532778
Rhyan Clark Randriamisaina <rhrandri@c1r6s16.42antananarivo.mg>
2026-02-06 14:31:48 +0300

    printf

--- ft_printf.h
+++ ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhrandri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 09:09:23 by rhrandri          #+#    #+#             */
-/*   Updated: 2026/02/06 12:58:45 by rhrandri         ###   ########.fr       */
+/*   Updated: 2026/02/06 13:43:10 by rhrandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(char const *format, ...);
+int	ft_printf(const char *format, ...);
 int	ft_putchar(char c);
 int	ft_putnbr(long n);
 int	ft_putnbr_hexa(unsigned long n, char up);
